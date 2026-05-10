bbe5fa52c2c28e058e83ffaff575f57824dd2205
Souvik Ray <roysouvikpaikpara@gmail.com>
2026-05-10 14:02:17 +0530

    done

--- server/server.ts
+++ server/server.ts
@@ -27,7 +27,8 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:3000',
-     'https://thumblify-server-self-three.vercel.app'
+    'https://thumblify-5fj3.vercel.app'
+    
   ],
   credentials: true
 }));
